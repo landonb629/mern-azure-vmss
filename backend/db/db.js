d52f597ab256ee89685696ae168b7560baecb23a
Landon Babay <landon.babay@outlook.com>
2023-01-24 00:30:54 -0500

    terraform deploy complete, just need to get a working backend deployment

--- backend/db/db.js
+++ backend/db/db.js
@@ -6,8 +6,8 @@ const dbConnection = () => {
     try {  
         return mongoose.connect("mongodb://10.10.2.4:10255/test?ssl=true&replicaSet=globaldb",{
             auth: { 
-                username: "mern-app-demo",
-                password: "M5ePiImFAvz71D4pbx7uQ7ug1V6pA8OauyY3umuP4d8JIPwCxc2AQAsGhr9w89tmuii8MuhSMDsQACDblLD2Mg=="
+                username: "",
+                password: ""
             },
             useNewUrlParser: true,
             useUnifiedTopology: true,
